a578af64845e56a40ef59f659fc3eb57f7851267
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-25 11:52:24 -0400

    Update train.py

--- trainingscript/train.py
+++ trainingscript/train.py
@@ -1,26 +1,28 @@
-from transformers import GPT2Tokenizer
+import pandas as pd
+from transformers import GPT2Tokenizer, TextDataset, GPT2Config, GPT2LMHeadModel, TrainingArguments, Trainer
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
-from transformers import TextDataset
-
 def load_dataset(file_path, tokenizer):
+    data = pd.read_csv(file_path)
+    data['combined'] = data['Item'] + ' | ' + data['Category']
+    text = '\n'.join(data['combined'].tolist())
+
+    with open('temp.txt', 'w') as f:
+        f.write(text)
+
     dataset = TextDataset(
         tokenizer=tokenizer,
-        file_path=file_path,
+        file_path='temp.txt',
         block_size=128
     )
     return dataset
 
-train_dataset = load_dataset("train.txt", tokenizer)
-valid_dataset = load_dataset("valid.txt", tokenizer)
-
-from transformers import GPT2Config, GPT2LMHeadModel
+train_dataset = load_dataset("mylist.csv", tokenizer)
+valid_dataset = load_dataset("mylist.csv", tokenizer)
 
 config = GPT2Config.from_pretrained("gpt2")
 model = GPT2LMHeadModel.from_pretrained("gpt2", config=config)
-
-from transformers import TrainingArguments
 
 training_args = TrainingArguments(
     output_dir="./output",
@@ -34,8 +36,6 @@ training_args = TrainingArguments(
     save_total_limit=2,
 )
 
-from transformers import Trainer
-
 trainer = Trainer(
     model=model,
     args=training_args,
@@ -47,4 +47,3 @@ trainer.train()
 
 model.save_pretrained("./my_finetuned_model")
 tokenizer.save_pretrained("./my_finetuned_model")
-
